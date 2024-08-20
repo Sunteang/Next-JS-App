ec4ed8b340e70e996583639e95882d47e694583f
Sunteang <129640164+Sunteang@users.noreply.github.com>
2024-08-20 13:40:25 +0700

    Delete app/components/organisms/filter-product directory

--- app/components/organisms/filter-product/filter-product.tsx
+++
@@ -1,7 +0,0 @@
-import { CSSProperties, PropsWithChildren } from "react";
-
-type ButtonProps = PropsWithChildren<{
-  onClick?: => void;
-  variant: 'Primary'| 'Secondary';
-  size: 'sm'|'md'|'lg';
-}>
